7b9e9946798efc3ce99f254c1c906a9078dcbfb6
Johnson <zxmagichacker@gmail.com>
2020-03-17 23:37:04 +0800

    feat: 设置跨域以及配置监听

--- app.ts
+++ app.ts
@@ -2,10 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
+const classRouter = require("./routes/class");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,7 +11,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// 设置跨域
+app.all("*", (req, res, next) => {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "http://www.zhangpeiyue.com");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers", "content-type");
+  //跨域允许的请求方式
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
-module.exports = app;
+app.use("/api", classRouter);
+
+app.listen(3000, () => {
+  console.log("\x1b[32m", "http://localhost:3000");
+});
