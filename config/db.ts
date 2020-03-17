676c1fcf20658e8ac385b8dd435987e41234858a
Johnson <zxmagichacker@gmail.com>
2020-03-17 23:46:35 +0800

    feat: mysql数据库配置

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,7 @@
 /**
- * mysql数据库配置
+ * mysql数据库配置与连接
  */
+const mysql = require("mysql2");
 module.exports = {
   mysql: {
     host: "127.0.0.1",
@@ -8,5 +9,16 @@ module.exports = {
     password: "zhang2684323",
     database: "onlineEducation",
     port: 3306
+  },
+  sqlConnect: function(sql) {
+    const connection = mysql.createConnection(this.mysql);
+    connection.connect(err => {
+      if (err) {
+        console.log("连接失败");
+        return false;
+      } else {
+        console.log("连接成功");
+      }
+    });
   }
 };
