26517e8601a3c4ecabe76b42790936aac2ab564e
Johnson <zxmagichacker@gmail.com>
2020-03-19 15:06:31 +0800

    feat: 实现分类管理的增删改查接口

--- app.ts
+++ app.ts
@@ -1,30 +1,31 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const classRouter = require("./routes/class");
-const app = express();
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const classRouter = require('./routes/class.ts')
+const app = express()
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // 设置跨域
-app.all("*", (req, res, next) => {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin", "http://www.zhangpeiyue.com");
-  //允许的header类型
-  res.header("Access-Control-Allow-Headers", "content-type");
-  //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+app.all('*', (req, res, next) => {
+  // 设置允许跨域的域名，*代表允许任意域名跨域
+  res.header('Access-Control-Allow-Origin', 'http://www.zhangpeiyue.com')
+  // 允许的header类型
+  res.header('Access-Control-Allow-Headers', 'content-type')
+  // 跨域允许的请求方式
+  res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
 
-app.use("/api", classRouter);
+// 挂载中间件方法到路径上
+app.use('/class', classRouter)
 
 app.listen(3000, () => {
-  console.log("\x1b[32m", "http://localhost:3000");
-});
+  console.log('\x1b[32m', 'http://localhost:3000')
+})
--- routes/class.ts
+++ routes/class.ts
@@ -1,18 +1,81 @@
 /**
  * 分类管理
  */
-export {};
-const express = require("express");
-const router = express.Router();
-const database = require("../config/db");
+// export {};
+const express = require('express')
+const router = express.Router()
+const database = require('../config/db.ts')
+const utils = require('../utils/index.ts')
 // 查询分类
-router.get("/getClass", (req, res) => {
-  database.sqlConnect();
-});
+router.get('/getClass', (req, res) => {
+  // 获取请求参数
+  let { className, createTime } = req.query
+  // TODO:待优化
+  let sql = 'select * from category_tbl'
+  if (className && !createTime) {
+    sql += ' where cate_name = ?'
+    createTime = ''
+  } else if (!className && createTime) {
+    sql += ' where create_time = ?'
+    className = ''
+  } else if (className && createTime) {
+    sql += ' where cate_name = ? or create_time = ?'
+  }
+  database.sqlConnect(sql, [className, createTime], (err, result) => {
+    if (err) {
+      res.send('查询失败' + err)
+    } else {
+      result.forEach(item => {
+        item.create_time = utils.formatDate(item.create_time)
+      })
+      res.json(utils.formatSuccessRes(result))
+    }
+  })
+})
 // 添加分类
-router.post("/addClass", (req, res) => {});
+router.post('/addClass', (req, res) => {
+  const { className, classSort } = req.body
+  const sql = 'insert into category_tbl (cate_name,cate_sort) values (?,?)'
+  database.sqlConnect(sql, [className, classSort], (err, result) => {
+    if (err) {
+      res.send('插入失败' + err)
+    } else {
+      res.send({
+        code: 0,
+        msg: '插入成功'
+      })
+    }
+  })
+})
 // 更新分类
-router.post("/updateClass", (req, res) => {});
+router.post('/updateClass', (req, res) => {
+  const { classId, className, classSort } = req.body
+  const sql =
+    'update category_tbl set cate_name = ?, cate_sort = ? where cate_id = ?'
+  database.sqlConnect(sql, [className, classSort, classId], (err, result) => {
+    if (err) {
+      res.send('更新失败' + err)
+    } else {
+      res.send({
+        code: 0,
+        msg: '更新成功'
+      })
+    }
+  })
+})
 // 删除分类
-router.delete("deleteClass", (req, res) => {});
-module.exports = router;
+router.delete('/deleteClass', (req, res) => {
+  const { classId } = req.query
+  const sql = 'delete from category_tbl where cate_id = ?'
+  database.sqlConnect(sql, [classId], (err, result) => {
+    if (err) {
+      res.send('删除失败' + err)
+    } else {
+      res.send({
+        code: 0,
+        msg: '删除成功'
+      })
+    }
+  })
+})
+module.exports = router
