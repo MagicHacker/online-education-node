3d01b76db032a8b7e88da9218730d9bd1b53b36e
Johnson <zxmagichacker@gmail.com>
2020-03-20 23:13:54 +0800

    feat: 引入ts和prettier相关的库，并进行配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: ["standard", "plugin:@typescript-eslint/recommended"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
   env: { node: true },
   rules: {
-    "@typescript-eslint/ban-ts-ignore": 0,
-    "@typescript-eslint/no-var-requires": 0
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-var-requires': 0
   }
-};
+}
