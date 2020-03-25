e20783738de6bc768c3117bead9fbb489b5841ec
Johnson <zxmagichacker@gmail.com>
2020-03-25 23:57:51 +0800

    fix: 跨域域名

--- app.ts
+++ app.ts
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // 设置跨域
 app.all('*', (req, res, next) => {
   // 设置允许跨域的域名，*代表允许任意域名跨域
-  res.header('Access-Control-Allow-Origin', 'http://www.zhangpeiyue.com')
+  res.header('Access-Control-Allow-Origin', '*')
   // 允许的header类型
   res.header('Access-Control-Allow-Headers', 'content-type')
   // 跨域允许的请求方式
