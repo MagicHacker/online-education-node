298ad4d27eb643eab223166876005bbe74077d56
Johnson <zxmagichacker@gmail.com>
2020-03-26 18:11:44 +0800

    feat: 成功响应添加ts接口

--- utils/index.ts
+++ utils/index.ts
@@ -3,28 +3,38 @@
  */
 // 日期格式化
 const formatDate = time => {
-  const date = new Date(time);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDay();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const senond = date.getSeconds();
-  return `${year}-${month <= 10 ? "0" + month : month}-${
-    day <= 10 ? "0" + day : day
-  } ${hours <= 10 ? "0" + hours : hours}:${
-    minutes <= 10 ? "0" + minutes : minutes
-  }:${senond <= 10 ? "0" + senond : senond}`;
-};
+  const date = new Date(time)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDay()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const senond = date.getSeconds()
+  return `${year}-${month <= 10 ? '0' + month : month}-${
+    day <= 10 ? '0' + day : day
+  } ${hours <= 10 ? '0' + hours : hours}:${
+    minutes <= 10 ? '0' + minutes : minutes
+  }:${senond <= 10 ? '0' + senond : senond}`
+}
 // 格式化成功响应
-const formatSuccessRes = result => {
+interface SuccessResult {
+  code: number
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  result: any
+  total: number
+  pageNum: string
+  pageSize: string
+}
+const formatSuccessRes = (result, total, pageNum, pageSize): SuccessResult => {
   return {
     code: 0,
     result,
-    total: result.length
-  };
-};
+    total,
+    pageNum,
+    pageSize
+  }
+}
 module.exports = {
   formatDate,
   formatSuccessRes
-};
+}
