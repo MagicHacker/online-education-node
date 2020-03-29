c9a5bd20fa362f9dc854e5ebc52864ef7e175b2f
Johnson <zxmagichacker@gmail.com>
2020-03-29 23:39:45 +0800

    feat: 收入表和教师表的关联查询

--- routes/salary.ts
+++ routes/salary.ts
@@ -1,57 +1,60 @@
 /**
  * 收入管理
  */
-const express = require('express')
-const router = express.Router()
-const database = require('../config/db.ts')
-const utils = require('../utils/index.ts')
+const express = require("express");
+const router = express.Router();
+const database = require("../config/db.ts");
+const utils = require("../utils/index.ts");
 
 // 查询收入
-router.get('/getSalarys', (req, res) => {
-  const { teacherName, dealType, dealTime } = req.query
-  // TODO:待优化
-  let sql = 'select * from sal_tbl'
-  let data = []
-  if (!teacherName && dealType && !dealTime) {
-    sql += ' where sal_type = ?'
-    data = [dealType]
-  } else if (!teacherName && !dealType && dealTime) {
-    sql += ' where deal_time = ?'
-    data = [dealTime]
-  } else if (!teacherName && dealType && dealTime) {
-    sql += ' where sal_type = ? and deal_time = ?'
-    data = [dealType, dealTime]
+router.get("/getSalarys", async (req, res) => {
+  const {
+    teacherName = "",
+    dealType = "",
+    dealTime = "",
+    pageNum,
+    pageSize
+  } = req.query;
+  // 关联查询，先查询所有需要字段，再根据条件进行筛选
+  const sql =
+    "select sal.*,(sal.sal_sum - sal_deal) as sal_after,tea.name,tea.phone from sal_tbl sal left join teacher_tbl tea on sal.teacher_id = tea.phone where tea.name like ? and sal.sal_type like ? and sal.deal_time like ? limit ?,?";
+  const totalSql = "select count(*) as total from sal_tbl";
+  const total = await database.sqlConnect(totalSql, []);
+  const result = await database.sqlConnect(sql, [
+    `%${teacherName}%`,
+    `%${dealType}%`,
+    `%${dealTime}%`,
+    (parseInt(pageNum) - 1) * parseInt(pageSize),
+    parseInt(pageSize)
+  ]);
+  if (result) {
+    result.forEach(item => {
+      item.deal_time = utils.formatDate(item.deal_time);
+    });
+    res.json(utils.formatSuccessRes(result, total[0].total, pageNum, pageSize));
+  } else {
+    res.send("查询失败" + result);
   }
-  database.sqlConnect(sql, data, (err, result) => {
-    if (err) {
-      res.send('查询失败' + err)
-    } else {
-      result.forEach(item => {
-        item.deal_time = utils.formatDate(item.deal_time)
-      })
-      res.json(utils.formatSuccessRes(result))
-    }
-  })
-})
+});
 
 // 添加收入
-router.post('/addSalary', (req, res) => {
-  const { salaryId, dealType, dealSalary, salarySum, teacherId } = req.body
+router.post("/addSalary", (req, res) => {
+  const { salaryId, dealType, dealSalary, salarySum, teacherId } = req.body;
   const sql =
-    'insert into sal_tbl (sal_id,sal_type,sal_deal,sal_sum,teacher_id) values(?,?,?,?,?)'
+    "insert into sal_tbl (sal_id,sal_type,sal_deal,sal_sum,teacher_id) values(?,?,?,?,?)";
   database.sqlConnect(
     sql,
     [salaryId, dealType, dealSalary, salarySum, teacherId],
     (err, result) => {
       if (err) {
-        res.send('插入失败' + err)
+        res.send("插入失败" + err);
       } else {
         res.send({
           code: 0,
-          msg: '插入成功'
-        })
+          msg: "插入成功"
+        });
       }
     }
-  )
-})
-module.exports = router
+  );
+});
+module.exports = router;
