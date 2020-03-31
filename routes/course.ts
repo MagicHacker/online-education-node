68efdd7b8d6f85bdea9294dcb14011889c66aff6
Johnson <zxmagichacker@gmail.com>
2020-03-31 14:21:11 +0800

    feat: 课程管理的模糊，分页，多表关联查询

--- routes/course.ts
+++ routes/course.ts
@@ -1,85 +1,68 @@
 /**
  * 课程管理
  */
-const express = require('express')
-const router = express.Router()
-const database = require('../config/db.ts')
-const utils = require('../utils/index.ts')
+const express = require("express");
+const router = express.Router();
+const database = require("../config/db.ts");
+const utils = require("../utils/index.ts");
 
 // 查询课程
-router.get('/getCourses', (req, res) => {
-  const { courseName, teacherName, oneLevel, twoLevel, createTime } = req.query
-  let sql = 'select * from course_tbl'
-  let data = []
-  if (courseName && !teacherName && !oneLevel && !twoLevel && !createTime) {
-    sql += ' where name = ?'
-    data = [courseName]
-  } else if (
-    !courseName &&
-    !teacherName &&
-    oneLevel &&
-    !twoLevel &&
-    !createTime
-  ) {
-    sql += ' where one_level = ?'
-    data = [oneLevel]
-  } else if (
-    !courseName &&
-    !teacherName &&
-    !oneLevel &&
-    twoLevel &&
-    !createTime
-  ) {
-    sql += ' where two_level = ?'
-    data = [twoLevel]
-  } else if (
-    !courseName &&
-    !teacherName &&
-    !oneLevel &&
-    !twoLevel &&
-    createTime
-  ) {
-    sql += ' where create_time = ?'
-    data = [createTime]
-  } else if (courseName && teacherName && oneLevel && twoLevel && createTime) {
-    sql += ' where name = ? and one_level = ? and two_level and create_time = ?'
-    data = [courseName, oneLevel, twoLevel, createTime]
+router.get("/getCourses", async (req, res) => {
+  const {
+    courseName = "",
+    teacherName = "",
+    oneLevel = "",
+    twoLevel = "",
+    createTime = "",
+    pageNum,
+    pageSize
+  } = req.query;
+  const totalSql = "select count(*) as total from course_tbl";
+  const sql =
+    "select course.*,teacher.name as teacherName from course_tbl course left join teac_course_tbl teaCourse on course.course_id = teaCourse.course_id left join teacher_tbl teacher on teaCourse.teacher_id = teacher.phone where course.name like ? and teacher.name like ? and one_level like ? and two_level like ? and create_time like ? limit ?,?";
+  const total = await database.sqlConnect(totalSql, []);
+  const result = await database.sqlConnect(sql, [
+    `%${courseName}%`,
+    `%${teacherName}%`,
+    `%${oneLevel}%`,
+    `%${twoLevel}%`,
+    `%${createTime}%`,
+    (parseInt(pageNum) - 1) * parseInt(pageSize),
+    parseInt(pageSize)
+  ]);
+  if (result) {
+    result.forEach(item => {
+      item.createTime = utils.formatDate(item.create_time);
+    });
+    res.json(utils.formatSuccessRes(result, total[0].total, pageNum, pageSize));
+  } else {
+    res.send("查询失败" + result);
   }
-  database.sqlConnect(sql, data, (err, result) => {
-    if (err) {
-      res.send('查询失败' + err)
-    } else {
-      result.forEach(item => {
-        item.create_time = utils.formatDate(item.create_time)
-      })
-      res.json(utils.formatSuccessRes(result))
-    }
-  })
-})
+});
 
 // 添加课程
-router.post('/addCourse', (req, res) => {
-  const { courseName, oneLevel, twoLevel, grade, price, courseUnit } = req.body
+router.post("/addCourse", (req, res) => {
+  const { courseName, oneLevel, twoLevel, grade, price, courseUnit } = req.body;
   const sql =
-    'insert into course_tbl (name,one_level,two_level,grade,price,course_unit) values(?,?,?,?,?,?)'
+    "insert into course_tbl (name,one_level,two_level,grade,price,course_unit) values(?,?,?,?,?,?)";
   database.sqlConnect(
     sql,
     [courseName, oneLevel, twoLevel, grade, price, courseUnit],
     (err, result) => {
       if (err) {
-        res.send('插入失败' + err)
+        res.send("插入失败" + err);
       } else {
         res.send({
           code: 0,
-          msg: '插入成功'
-        })
+          msg: "插入成功"
+        });
       }
     }
-  )
-})
+  );
+});
 
 // 更新课程
-router.post('/updateCourse', (req, res) => {
+router.post("/updateCourse", (req, res) => {
   const {
     courseName,
     oneLevel,
@@ -88,22 +71,22 @@ router.post('/updateCourse', (req, res) => {
     price,
     courseUnit,
     courseId
-  } = req.body
+  } = req.body;
   const sql =
-    'update course_tbl set name = ?,one_level = ?, two_level = ?,grade = ?, price = ?, course_unit = ? where course_id = ?'
+    "update course_tbl set name = ?,one_level = ?, two_level = ?,grade = ?, price = ?, course_unit = ? where course_id = ?";
   database.sqlConnect(
     sql,
     [courseName, oneLevel, twoLevel, grade, price, courseUnit, courseId],
     (err, result) => {
       if (err) {
-        res.send('更新失败' + err)
+        res.send("更新失败" + err);
       } else {
         res.send({
           code: 0,
-          msg: '更新成功'
-        })
+          msg: "更新成功"
+        });
       }
     }
-  )
-})
-module.exports = router
+  );
+});
+module.exports = router;
